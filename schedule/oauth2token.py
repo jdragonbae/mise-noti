71e9ca4ac4a65dcad947722c681d4bd06347a141
Ubuntu <ubuntu@ip-172-31-88-38.ec2.internal>
2018-07-20 23:33:26 +0000

    Fix aouth2token.py

--- schedule/oauth2token.py
+++ schedule/oauth2token.py
@@ -39,7 +39,7 @@ def get_token(code):
         if 'location' in event:
             location = event['location'].split(',')[0].strip()
             micro_dust = get_micro_dust(location)
-            Schedule.objects.create(scheTitle=event['summary'],location=location, time=start, micro_dust=micro_dust)
+            Schedule.objects.create(scheTitle=event['summary'],location=location, date=start, micro_dust=micro_dust)
             
 
 
